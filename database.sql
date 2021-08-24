06b128fce0692f41f1a831e1093bf8d566d67009
Heriot <heriotrandri@gmail.com>
2021-08-24 21:43:04 +0200

    base2

--- database.sql
+++ database.sql
@@ -13,3 +13,40 @@ create table caisse(
 
 insert into caisse(nom) values('Caisse n°1'),
     ('Caisse n°2'),('Caisse n°3'),('Caisse n°4'),('Caisse n°5'),('Caisse n°6');
+create table categorie(
+  idcategorie int not null primary key AUTO_INCREMENT,
+  nom varchar(250) unique
+);
+
+create table  produit(
+  idproduit int not null primary key AUTO_INCREMENT,
+  nom varchar(250) unique,
+  prix double precision,
+  stock int,
+  check stock > 0,
+  ispromotion boolean
+);
+
+create table promotion(
+  idproduit int foreign key references produit(idproduit),
+  prixpromotion double precision
+);
+
+create table relation(
+  idproduit int foreign key references produit(idproduit),
+  idcategorie int foreign key references categorie(idcategorie)
+);
+
+create table achat(
+  idcaisse int foreign key references caisse(idcaisse),
+  datedachat timestamp,
+  prixTotal double precision,
+);
+
+create table detailsachat(
+  idachat int foreign key references achat(idachat),
+  idproduit int foreign key references produit(idproduit),
+  prixUnitaire double precision,
+  quantite int,
+  total double precision
+);
