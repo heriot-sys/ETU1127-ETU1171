376d99456a7750bb66acbee79b5d0fc93501c768
Heriot <heriotrandri@gmail.com>
2021-08-24 23:53:39 +0200

    data

--- database.sql
+++ database.sql
@@ -34,7 +34,11 @@ create table  produit(
 insert into produit(nom,image,idcategorie,prix,stock) values('Elseve Huile','Elseve huile.jpg',1,7000.0,40),('Maybeline make up','Maybelline NY makeup.jpg',1,8000.0,30)
   ,('Boss botteled','boss botteled.jpg',10000.0,30),('Shampoing solide aux oeufs','shampoing oeuf.jpg',5000.0,20);
 insert into produit(nom,image,idcategorie,prix,stock) values('Coca cola PM','coca cola.jpg',4,1000.0,50),('Jus Fanta GM','fanta.jpg',4,4000.0,50),('Nouilles Matsiro','matsiro.jpg',4,1000.0,50),
-  ()
+  ('La rizière carton','sac riz.jpg',4,12000.0,50);
+insert into produit(nom,image,idcategorie,prix,stock) values('Couche pampers','pampers.jpg',3,6000.0,20),('Attitude','attitude.jpg',3,8000.0,30),('Mustella','mustella.jpg',3,5000.0,30),
+  ('Mam easy biberon','biberon.jpg',3,5000.0,20);
+insert into produit(nom,image,idcategorie,prix,stock) values('Ballon de foot','foot.jpg',2,10000.0,10),('Ballon de basket molten','molten.jpg',2,10000.0,10),('Ballon de rugby','rugby.jpg',2,8000.0,10),
+  ('Ballon de volley mikasa','mikasa.jpg',2,8000.0,10);
 
 create table promotion(
   idproduit int foreign key references produit(idproduit),
